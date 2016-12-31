293eaa46627ddf1bd1ba2e702c7b2c648fc199f1
littleEric <lyy6963@gmail.com>
2016-12-31 15:41:01 +0800

    新增断点续传功能

--- getimgs.py
+++ getimgs.py
@@ -1,11 +1,13 @@
 from bs4 import BeautifulSoup
-import json,re,requests
+import json,re,requests,os
 ##图片标题下标
 temp = 0
 ##offset值
 offset = 0
 #图片存放文件夹路径（末尾不带/）
-storepath = "/Volumes/100G(HDD)/zhihu_res"
+storepath = "/Volumes/100G(HDD)/zhihu_res2"
+##储存目录中的文件列表
+dir_list = os.listdir(storepath)
 ##返回msg的src属性
 def getsrc(html):
     srcs = []
@@ -48,10 +50,35 @@ def get_imgs(id):
     for msg in msgs:
         for src in getsrc(msg):
             data = requests.get(src).content
-            filename = storepath +"/" + str(temp) + ".jpg"
+            filename = storepath +"/" + str(offset) + "-" + str(temp) + ".jpg"
             writefile(filename, data)
             temp = temp + 1
             print("==============" + filename + " has been saved=================")
+    temp = 0
     offset = offset+10
-while 1:
-    get_imgs(52570020)
+##过滤非法文件
+def rm_other_files(dirlist):
+    ##传入dir_list引用
+    for i in range(0,len(dirlist)-1):
+        if not re.match("\d+-\d+.jpg",dirlist[i]):
+            del dirlist[i]
+##根据文件名计算出index
+def getindex(filename):
+    offset = int(filename[:filename.index('-')])
+    name_index = int(filename[(filename.index('-')+1):filename.index(".")])
+    ##消除offset受到name_index的影响
+    return offset*400+name_index
+def get_last_offset(list):
+    last_file_name = list[len(list)-1]
+    offset = int(last_file_name[:last_file_name.index("-")])
+    return offset
+##程序运行入口
+def runpro():
+    global dir_list,offset
+    rm_other_files(dir_list)
+    # print(getindex("450-1.jpg"))
+    dir_list.sort(key=lambda dir: getindex(dir))
+    offset = get_last_offset(dir_list)
+    while 1:
+        get_imgs(52570020)
+runpro()
